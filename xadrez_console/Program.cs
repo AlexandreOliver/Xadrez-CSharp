736498ccc1bc499d1fc0ea6fded3077dd794bda3
Alexandre Oliver <Alexandreferreiradeoliveira40@gmail.com>
2023-01-31 19:09:06 -0300

    Melhorando metodo imprimirTabuleiro

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -8,12 +8,12 @@ Tabuleiro tab = new Tabuleiro(8, 8);
 
 tab.colocarPeca(new Torre(tab, Cor.Branco), new Posicao(0, 0));
 tab.colocarPeca(new Rei(tab, Cor.Branco), new Posicao(1, 3));
-tab.colocarPeca(new Torre(tab, Cor.Branco), new Posicao(3, 3));
-tab.colocarPeca(new Rei(tab, Cor.Branco), new Posicao(4, 0));
+tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(3, 3));
+tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(4, 0));
 
 
 
-Tela.ImprimirTela(tab);
+Tela.ImprimirTabuleiro(tab);
 }
 catch (TabuleiroException e)
 {
--- xadrez_console/Tela.cs
+++ xadrez_console/Tela.cs
@@ -4,10 +4,11 @@ namespace xadrez_console;
 
 class Tela
 {
-    public static void ImprimirTela(Tabuleiro tab)
+    public static void ImprimirTabuleiro(Tabuleiro tab)
     {
         for (int i = 0; i < tab.Linhas; i++)
         {
+            Console.Write(8 - i + " |");
             for (int j = 0; j < tab.Colunas; j++)
             {
                 if (tab.peca(i, j) == null)
@@ -16,11 +17,29 @@ class Tela
                 }
                 else
                 {
-                    Console.Write($"{tab.peca(i, j)}  ");
+                    Tela.imprimirPeca(tab.peca(i, j));
+                    Console.Write("  ");
+
                 }
                
             }
             Console.WriteLine();
         }
+        Console.WriteLine("   ----------------------");
+        Console.WriteLine("   a  b  c  d  e  f  g  h");
+    }
+
+    public static void imprimirPeca(Peca peca)
+    {
+        if (peca.Cor == Cor.Branco)
+            Console.Write(peca);
+        else
+        {
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(peca);
+            Console.ForegroundColor = aux;
+        }
+
     }
 }
