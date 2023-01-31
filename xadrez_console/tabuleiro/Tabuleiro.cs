2d0e0aaa3293ee064686e6f410fc38facf93719e
Alexandre Oliver <Alexandreferreiradeoliveira40@gmail.com>
2023-01-31 15:41:03 -0300

    Implementando metodo que imprime o Tabuleiro na Tela

--- xadrez_console/tabuleiro/Tabuleiro.cs
+++ xadrez_console/tabuleiro/Tabuleiro.cs
@@ -12,4 +12,9 @@ class Tabuleiro
         Colunas = colunas;
         pecas = new Peca[linhas, colunas];
     }
+
+    public Peca peca(int linha, int coluna)
+    {
+        return pecas[linha, coluna];
+    }
 }
